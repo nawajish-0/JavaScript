36d2c1b8a6607ad7d979ef00c0ea3b22c2a18034
nawajish-0 <itsnawajish020@gmail.com>
2024-05-25 14:48:56 +0530

    update

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log("Nawajish");
+console.log("Nawajish")
+const accountId = 1490
+let accoutnEmail ="lol@gmail.com"
+var accountPass = "12345"
+accountCity = "Delhi" // variable can be declared like this but bad practice
+let accountState;  // undefined
+
+//accountId = 2 // not allowed , as accountId is const
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+ 
+*/
+console.table([accountPass,accountState,accoutnEmail,accountId])
