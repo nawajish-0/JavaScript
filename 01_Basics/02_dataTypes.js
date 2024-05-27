8e9aff71c1a35d25ca91cae7ac1b379d0432dfa2
nawajish-0 <itsnawajish020@gmail.com>
2024-05-27 18:40:31 +0530

    update

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code newer version
+
+// alert("hello")
+// alert( 3 + 4 ) // we are using nodejs, not browser
+
+console.log(3
+
+    // readability should be high
+)
+console.log("hi") 
+
+let name = "nawajish" //string
+let age = 23        // number
+let isLoggedIn = false // bool
+let state1 ; //undefined
+let state = null // empty 
+
+/*primitive datatypes
+
+     number => 2 to power 53
+     bigint 
+     string => ""
+     boolean => true/false
+     null => standalone value (null is an object datatype)
+     undefined =>
+     symbol => unique
+*/
+
+// object 
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
